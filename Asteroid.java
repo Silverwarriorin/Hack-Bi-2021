fa10193510da0eccb0a4a39a01a25ac6572ebd30
Zachary Haggard <37089160+AgentUkulele@users.noreply.github.com>
2021-01-23 14:56:50 -0500

    Update Asteroid.java

--- Asteroid.java
+++ Asteroid.java
@@ -4,18 +4,22 @@ import java.awt.event.*;
 
 public class Asteroid extends JPanel{
 
+    //setting bounds for spawning max = 700 min = 50
     int xmax = 700, xmin = 50;
     int x = (int)(Math.random() * (xmax - xmin + 1) + xmin), y = (int)(Math.random() * (xmax - xmin + 1) + xmin);
     
+    //setting bounds for size max = 50 min = 15
     int max = 50, min = 15;
     int w = (int)(Math.random() * (max - min + 1) + min), h = w;
  
+    //I have no clue how to make this work
     public void glide(Asteroid arg)
     {
         arg.x += (int)(Math.random() * (10) + 1);
         arg.y += (int)(Math.random() * (10) + 1);
     }
 
+    //spawn method
     public void spawn(Graphics arg)
     {
         arg.setColor(Color.BLACK);
