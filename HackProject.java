9fd63ee49a8b4d5f914903f0f96cad15802b334f
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2021-01-23 11:38:10 -0500

    Updated and added keylistener

--- HackProject.java
+++ HackProject.java
@@ -1,4 +1,36 @@
-public class HackProject
+import javax.swing.JPanel;
+import javax.swing.*;
+
+
+
+public class HackProject extends JPanel 
 {
     
+    public static void main(String[] args)
+    {
+                     
+    }
+    
+    
+    private static class Key extends KeyAdapter
+    { 
+      public void keyPressed(KeyEvent e)
+      {
+             if(e.getKeyCode() == KeyEvent.VK_W){               
+                
+                }
+             if(e.getKeyCode() == KeyEvent.VK_A){
+               
+               }
+              if(e.getKeyCode() == KeyEvent.VK_D){
+               
+               }
+             if(e.getKeyCode() == KeyEvent.VK_S){
+               
+               }
+      }
+   }
+
+
+
 }
