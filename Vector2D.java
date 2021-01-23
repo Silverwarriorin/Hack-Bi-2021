92b796c4468f4d36acb47fd7125e48e42ba34191
giulianij <giulianij@gonzaga.org>
2021-01-23 13:51:07 -0500

    Create Vector2D.java

---
+++ Vector2D.java
@@ -0,0 +1,67 @@
+
+
+
+public class Vector2D
+{
+    double x, y;
+    
+    public Vector2D()
+    {
+    	
+    }
+    
+    public void setVector(double speed, double radians)
+    {
+    	x = Math.cos(radians)*speed;
+    	y = Math.sin(radians)*speed;
+    }
+    
+    public Vector(double x, double y)
+    {
+    	this.x=x;
+    	this.y=y;
+    }
+    
+    public double getSpeed()
+    {
+    	return y/Math.sin(Math.atan(y/x));
+    }
+    
+    public double getDirection()
+    {
+    	return Math.atan(y/x);
+    }
+    
+    public double getX()
+    {
+    	return x;
+    }
+    
+    public double getY()
+    {
+    	return y;
+    }
+    
+    public void setSpeed(double s)
+    {
+    	x = Math.cos(getDirection())*s;
+    	y = Math.sin(getDirection())*s;
+    }
+    
+    public void setDirection(double radians)
+    {
+    	x = Math.cos(radians)*getSpeed();
+    	y = Math.sin(radians)*getSpeed();
+    }
+    
+    public Vector clone()
+    {
+    	return new Vector(x,y);
+    }
+    
+    public String toString()
+    {
+    	return "y: " + x + " y: " + y + " speed: " + getSpeed() + " direction: " + Math.toDegrees(getDirection());
+    }
+
+}
