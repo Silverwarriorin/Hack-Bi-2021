ad7c483cfece990bccd327cdd3d46b56738f8d2c
Krish M <848223@lcps.org>
2021-01-24 00:19:17 -0500

    Update Laser.java
    Laser changes to constructor and new methods. Possibility of being bugged.

--- Laser.java
+++ Laser.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,54 +12,12 @@ public class Laser extends JPanel{
 	private int height;
 	private Vector2D vector;
 	
-	public Laser(Spaceship player) {
-		x = player.getX();
-		y = player.getY();
-		vector = new Vector2D(x, y);
-		direction = vector.getDirection();
-		speed = vector.getSpeed();
+	public Laser(int x, int y, Vector2D vect) {
+		this.x = x;
+		this.y = y;
+		vector = vect.clone();
 		width = 5;
 		height = 5;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	
-	public double getSpeed() {
-		return vector.getSpeed();
-	}
-	
-	public double getDirection() {
-		return vector.getDirection();
 	}
 
 	public boolean edgeCollision (int rightEdge, int bottomEdge) {
@@ -81,11 +37,15 @@ public class Laser extends JPanel{
 	}
 	
 	public boolean hitAsteroid (Asteroid asteroid) {
-		double distanceToAsteroid = Math.sqrt(Math.pow(asteroid.getX() - x, 2) + Math.pow(asteroid.getY() - y, 2));
-		if (distanceToAsteroid <= asteroid.getWidth()) {
+		if (asteroid.getBoundingBox().contains(x, y)) {
 			return true;
 		}
 		return false;
+	}
+	
+	public void drive() {
+		x += vector.getX();
+		y += vector.getY();
 	}
 	
 	public void draw(Graphics g) {		
